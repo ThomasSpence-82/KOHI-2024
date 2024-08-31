de3a609ec37358e890daf4b2078d9722088de930
ThomasSpence-82 <tespence@outlook.com>
2024-08-31 13:30:19 +0100

    #78 - Pixel perfect object shading...

--- engine/src/core/event.h
+++ engine/src/core/event.h
@@ -140,8 +140,8 @@ typedef enum system_event_code {
 
     /** @brief Mouse moved.
      * Context usage:
-     * u16 x = data.data.u16[0];
-     * u16 y = data.data.u16[1];
+     * u16 x = data.data.i16[0];
+     * u16 y = data.data.i16[1];
      */
     EVENT_CODE_MOUSE_MOVED = 0x06,
 
@@ -174,6 +174,18 @@ typedef enum system_event_code {
     EVENT_CODE_DEBUG3 = 0x13,
     /** @brief Special-purpose debugging event. Context will vary over time. */
     EVENT_CODE_DEBUG4 = 0x14,
+
+    /** @brief The hovered-over object id, if there is one.
+     * Context usage:
+     * i32 id = context.data.u32[0]; - will be INVALID ID if nothing is hovered over.
+     */
+    EVENT_CODE_OBJECT_HOVER_ID_CHANGED = 0x15,
+
+    /** 
+     * @brief An event fired by the renderer backend to indicate when any render targets
+     * associated with the default window resources need to be refreshed (i.e. a window resize)
+     */
+    EVENT_CODE_DEFAULT_RENDERTARGET_REFRESH_REQUIRED = 0x16,
 
     /** @brief The maximum event code that can be used internally. */
     MAX_EVENT_CODE = 0xFF
