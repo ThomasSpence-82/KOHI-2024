f6b694bf51c851eddf0d795e9bac51bae2eca077
ThomasSpence-82 <tespence@outlook.com>
2024-09-01 07:00:59 +0100

    kvars

--- testbed/src/game.h
+++ testbed/src/game.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <defines.h>
-#include <game_types.h>
+#include <application_types.h>
 #include <math/math_types.h>
 #include <systems/camera_system.h>
 
 // TODO: temp
 #include <resources/skybox.h>
 #include <resources/ui_text.h>
+#include <core/clock.h>
+#include <core/keymap.h>
 
 typedef struct game_state {
     f32 delta_time;
@@ -16,6 +18,10 @@ typedef struct game_state {
     u16 width, height;
 
     frustum camera_frustum;
+
+    clock update_clock;
+    clock render_clock;
+    f64 last_update_elapsed;
 
     // TODO: temp
     skybox sb;
@@ -31,19 +37,24 @@ typedef struct game_state {
 
     // The unique identifier of the currently hovered-over object.
     u32 hovered_object_id;
+
+    keymap console_keymap;
+
+    u64 alloc_count;
+    u64 prev_alloc_count;
     // TODO: end temp
 } game_state;
 
 struct render_packet;
 
-b8 game_boot(struct game* game_inst);
+b8 game_boot(struct application* game_inst);
 
-b8 game_initialize(game* game_inst);
+b8 game_initialize(application* game_inst);
 
-b8 game_update(game* game_inst, f32 delta_time);
+b8 game_update(application* game_inst, f32 delta_time);
 
-b8 game_render(game* game_inst, struct render_packet* packet, f32 delta_time);
+b8 game_render(application* game_inst, struct render_packet* packet, f32 delta_time);
 
-void game_on_resize(game* game_inst, u32 width, u32 height);
+void game_on_resize(application* game_inst, u32 width, u32 height);
 
-void game_shutdown(game* game_inst);
+void game_shutdown(application* game_inst);
